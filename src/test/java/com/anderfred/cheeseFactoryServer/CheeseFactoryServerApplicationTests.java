5b2b5f22b0319ebc29728a006eebfcd488db7dac
Egor Belkov <Subgorn@gmail.com>
2019-01-23 19:29:09 +0700

    secondd

---
+++ src/test/java/com/anderfred/cheeseFactoryServer/CheeseFactoryServerApplicationTests.java
@@ -0,0 +1,17 @@
+package com.anderfred.cheeseFactoryServer;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CheeseFactoryServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
